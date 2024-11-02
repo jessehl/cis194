cf488c352b1c9b5106ee1d1d6c2e8f28b12c48a9
jessehl <j.loor@hotmail.com>
2024-11-02 17:26:23 +0100

    give up, generalized solution exists but is not optimal

--- src/Week1.hs
+++ src/Week1.hs
@@ -28,23 +28,3 @@ hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
 hanoi 0 _ _ _ = []
 hanoi 1 a _ c = [(a, c)]
 hanoi n a b c = hanoi (n - 1) a c b ++ hanoi 1 a b c ++ hanoi (n - 1) b a c
-
--- Implementation where we have multiple spare pegs.
-hanoi' :: Int -> Int -> NonEmpty.NonEmpty Int -> Int -> [(Int, Int)]
-hanoi' 0 _ _ _ = []
-hanoi' 1 sourcePeg _ targetPeg = [(sourcePeg, targetPeg)]
-hanoi' nDisks sourcePeg sparePegs targetPeg
-  -- If enough spares, we move all but the last of the source disks onto the spare pegs.
-  -- Then, move the remaining source disk onto the target, followed by the pegs just placed on the spares.
-  | nDisks <= length sparePegs = map (sourcePeg,) sparesUsed ++ [(sourcePeg, targetPeg)] ++ map (,targetPeg) (reverse sparesUsed)
-  | otherwise =
-      -- Move n-1 disks to the first spare peg, using the target peg as extra spare.
-      hanoi' (nDisks - 1) sourcePeg (targetPeg NonEmpty.:| NonEmpty.tail sparePegs) (NonEmpty.head sparePegs)
-        ++
-        -- Move the top disk to the target peg.
-        [(sourcePeg, targetPeg)]
-        ++
-        -- Move n-1 disks from the first spare peg to the target, using the source as extra spare.
-        hanoi' (nDisks - 1) (NonEmpty.head sparePegs) (sourcePeg NonEmpty.:| NonEmpty.tail sparePegs) targetPeg
-  where
-    sparesUsed = NonEmpty.take (nDisks - 1) sparePegs
