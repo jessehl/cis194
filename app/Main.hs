bdded8171150440cd4dd44940174f401e020856d
jessehl <j.loor@hotmail.com>
2024-11-09 09:07:58 +0100

    add week 2 files

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,10 @@
 module Main where
 
-import  Week1
+import Week1
+import Week2.Week2
 
 main :: IO ()
 main = do
   print "Hello, Haskell!"
   print (doubleEveryOtherB [23,32])
+  print messageType
---
+++ src/Week2/Week2.hs
@@ -0,0 +1,6 @@
+module Week2.Week2 where 
+
+import Week2.Log 
+
+messageType :: MessageType
+messageType = Info
