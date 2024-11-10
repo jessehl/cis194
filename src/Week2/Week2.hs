97826bfda5d8e315f9b4b200ed55478985f24174
jessehl <j.loor@hotmail.com>
2024-11-10 14:20:30 +0100

    expose function that is easier to work with

--- src/Week2/Week2.hs
+++ src/Week2/Week2.hs
@@ -5,21 +5,24 @@ import Week2.Log
 import qualified Data.Bifunctor
 import Data.Char (isDigit, digitToInt)
 
-
 parseMessage :: String -> LogMessage
 parseMessage _ = undefined
 
 consumeInt :: Int -> [Char] -> Maybe(Int, [Char])
 consumeInt acc [] = Just(acc, []) 
-consumeInt acc (x1:xs)
-    | x1 == ' '   = Just(acc, [])
-    | isDigit x1  = consumeInt (digitToInt x1 + acc * 10) xs
-    | otherwise   = Nothing
+consumeInt acc (x:xs)
+    | x == ' '   = Just(acc, [])
+    | isDigit x  = consumeInt (digitToInt x + acc * 10) xs
+    | otherwise  = Nothing
+
+stripLeftInt :: [Char] -> Maybe(Int, [Char])
+stripLeftInt []  = Nothing
+stripLeftInt str = consumeInt 0 str
          
 
 getType :: String -> Maybe (MessageType, String)
 getType line = case line of
   'I' : ' ' : xs -> Just (Info, xs)
   'W' : ' ' : xs -> Just (Warning, xs)
-  'E' : ' ' : xs -> consumeInt 0 xs & fmap (Data.Bifunctor.first Error)
+  'E' : ' ' : xs -> stripLeftInt xs & fmap (Data.Bifunctor.first Error)
   _ -> Nothing
