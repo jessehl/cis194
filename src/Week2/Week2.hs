62d371f1586a23e8b8b87a79813de75f67ef613e
jessehl <j.loor@hotmail.com>
2024-11-10 10:29:52 +0100

    implemen getType

--- src/Week2/Week2.hs
+++ src/Week2/Week2.hs
@@ -1,6 +1,25 @@
-module Week2.Week2 where 
+module Week2.Week2 where
 
-import Week2.Log 
+import Data.Function ((&))
+import Week2.Log
+import qualified Data.Bifunctor
+import Data.Char (isDigit, digitToInt)
 
-messageType :: MessageType
-messageType = Info
+
+parseMessage :: String -> LogMessage
+parseMessage _ = undefined
+
+consumeInt :: Int -> [Char] -> Maybe(Int, [Char])
+consumeInt acc [] = Just(acc, []) 
+consumeInt acc (x1:xs)
+    | x1 == ' '   = Just(acc, [])
+    | isDigit x1  = consumeInt (digitToInt x1 + acc * 10) xs
+    | otherwise   = Nothing
+         
+
+getType :: String -> Maybe (MessageType, String)
+getType line = case line of
+  'I' : ' ' : xs -> Just (Info, xs)
+  'W' : ' ' : xs -> Just (Warning, xs)
+  'E' : ' ' : xs -> consumeInt 0 xs & fmap (Data.Bifunctor.first Error)
+  _ -> Nothing
