332ccb0e834ba8f89b22b4fbe8d939e2da5b92df
jessehl <j.loor@hotmail.com>
2024-12-21 18:02:40 +0100

    wip: monadic parser

--- app/Main.hs
+++ app/Main.hs
@@ -7,4 +7,6 @@ import Data.Foldable (traverse_)
 main :: IO ()
 main = do
    messages <- testParse parse 1000000 "resources/Week2/error.log"
-   traverse_ print (inorder (build messages))
+   _ <- traverse_ print (inorder (build messages))
+   print (run myResult "1232e234234234")
+
--- src/Week2/Week2.hs
+++ src/Week2/Week2.hs
@@ -16,23 +16,85 @@ data Tree a = Leaf
   | Node (Tree a) a (Tree a)
   deriving Foldable
 
+
+data ParseResult a = ParseError | Parsed a String
+  deriving Show
+
+newtype Parser a = Parser { run :: String -> ParseResult a }
+
+bind :: Parser a -> (a -> Parser b) -> Parser b
+bind p f = Parser $ \s ->
+  case run p s of
+    ParseError -> ParseError
+    Parsed a rest -> run (f a) rest
+
+create :: a -> Parser a
+create a = Parser { run = Parsed a }
+
+
+fmap1 :: (a -> b) -> Parser a -> Parser b 
+fmap1 f pa = Parser $ \s -> case run pa s of 
+      ParseError -> ParseError  
+      Parsed a c -> Parsed (f a) c
+
+instance Functor Parser where 
+  fmap = fmap1
+
+-- Applicative instance
+instance Applicative Parser where
+  pure :: a -> Parser a
+  pure a = Parser { run = Parsed a }
+
+  (<*>) :: Parser (a -> b) -> Parser a -> Parser b
+  (<*>) p a = Parser $ \s -> case run p s of 
+      ParseError    -> ParseError 
+      Parsed f rest -> run (fmap f a) rest
+
+
+instance Monad Parser where
+  return = pure
+
+  (>>=) :: Parser a -> (a -> Parser b) -> Parser b
+  (>>=) = bind
+
+
+getInt :: Parser Int
+getInt = create 1
+
+myResult:: Parser (Int, Int, Int) 
+myResult = do 
+  first  <- parseInt 
+  second <- parseInt 
+  third  <- parseInt 
+  create (first, second, third)
+
+parseInt :: Parser Int 
+parseInt = Parser $ \s -> case s of 
+   s : xs -> Parsed (digitToInt s) xs
+   _      -> ParseError
+
+
+
+
+
+
 parseMessage :: String -> LogMessage
-parseMessage line = fromMaybe (Unknown line) $ do 
+parseMessage line = fromMaybe (Unknown line) $ do
   (messageType, remainder1) <- getType line
   (timeStamp, remainder2) <- stripLeftInt remainder1
   Just (LogMessage messageType timeStamp remainder2)
- 
+
 consumeInt :: Int -> [Char] -> Maybe(Int, [Char])
-consumeInt acc [] = Just(acc, []) 
+consumeInt acc [] = Just (acc, [])
 consumeInt acc (x:xs)
-    | x == ' '   = Just(acc, xs)
+    | x == ' '   = Just (acc, xs)
     | isDigit x  = consumeInt (digitToInt x + acc * 10) xs
     | otherwise  = Nothing
 
 stripLeftInt :: [Char] -> Maybe(Int, [Char])
 stripLeftInt []  = Nothing
 stripLeftInt str = consumeInt 0 str
-         
+
 
 getType :: String -> Maybe (MessageType, String)
 getType line = case line of
@@ -44,10 +106,10 @@ getType line = case line of
 
 insert :: LogMessage -> Tree LogMessage -> Tree LogMessage
 insert (Unknown _) tree = tree
-insert newMessage@(LogMessage _ newTimestamp _) tree = case tree of 
+insert newMessage@(LogMessage _ newTimestamp _) tree = case tree of
    Leaf                  -> Node Leaf newMessage Leaf
    Node l (Unknown _) r  -> Node l newMessage r
-   Node l oldMessage@(LogMessage _ oldTimestamp _) r -> 
+   Node l oldMessage@(LogMessage _ oldTimestamp _) r ->
      if newTimestamp < oldTimestamp then Node (insert newMessage l) oldMessage r
      else Node l oldMessage (insert newMessage r)
 
