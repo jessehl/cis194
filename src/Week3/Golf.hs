b85457f6501b8041810985a64e9eed2e5352531f
jessehl <j.loor@hotmail.com>
2024-12-23 20:45:27 +0100

    first exercise

--- src/Week3/Golf.hs
+++ src/Week3/Golf.hs
@@ -1,3 +1,7 @@
-module Week3.Golf where 
+module Week3.Golf where
 
 skips :: [a] -> [[a]]
+skips xs = do 
+  (n, _) <- indexed
+  pure [val | (idx, val) <- indexed, idx `mod` n == 0]
+  where indexed = zip [1..] xs
