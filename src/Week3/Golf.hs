b902f06122e40f28e478fb159073ec19f4f1f35f
jessehl <j.loor@hotmail.com>
2024-12-24 18:34:19 +0100

    implement `localMaxima`

--- src/Week3/Golf.hs
+++ src/Week3/Golf.hs
@@ -1,4 +1,9 @@
 module Week3.Golf where
+import Data.List (sortBy)
+import Data.Ord (comparing, Down (Down))
 
 skips :: [a] -> [[a]]
 skips xs = [[val | (idx, val) <- zip [1..] xs, idx `mod` n == 0] | n <- fmap snd (zip xs [1..])]
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima lst = sortBy (comparing Data.Ord.Down) $ concatMap (\(left, this, right) -> ([this | this > left && this > right])) (zip3 (tail $ tail lst) (tail lst) lst)
