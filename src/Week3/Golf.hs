a9ca62959ac188d28d33ee43a9ef1134c79ed3f3
jessehl <j.loor@hotmail.com>
2024-12-24 17:55:54 +0100

    use nested list comp

--- src/Week3/Golf.hs
+++ src/Week3/Golf.hs
@@ -1,7 +1,4 @@
 module Week3.Golf where
 
 skips :: [a] -> [[a]]
-skips xs = do 
-  (n, _) <- indexed
-  pure [val | (idx, val) <- indexed, idx `mod` n == 0]
-  where indexed = zip [1..] xs
+skips xs = [[val | (idx, val) <- zip [1..] xs, idx `mod` n == 0] | n <- fmap snd (zip xs [1..])]
