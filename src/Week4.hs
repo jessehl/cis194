11f3a3181271f59e1cc8dae5796cc4b9fb53f1b3
jessehl <j.loor@hotmail.com>
2024-12-29 08:41:58 +0100

    start week 2

--- src/Week4.hs
+++ src/Week4.hs
@@ -9,3 +9,12 @@ fun1 (x:xs)
 
 fun1_ :: [Integer] -> Integer
 fun1_ xs = product $ (\x -> x -2) <$> filter even xs
+
+
+fun2 :: Integer -> Integer
+fun2 1 = 0
+fun2 n | even n = n + fun2 (n `div` 2)
+       | otherwise = fun2 (3 * n + 1)
+
+fun2_ :: Integer -> Integer 
+fun2_ = fun2
--- test/Week4Spec.hs
+++ test/Week4Spec.hs
@@ -7,4 +7,8 @@ spec :: Spec
 spec = do
   describe "fun1" $ do
     it "should work" $
-      fun1 [234,234,1,0,3] `shouldSatisfy` (==fun1_ [234,234,1,0,3] )
+      fun1 [234,234,1,0,3] `shouldSatisfy` (==fun1_ [234,234,1,0,3])
+
+  describe "fun2" $ do 
+    it "should work" $ 
+       fun2 2413354 `shouldSatisfy` (== fun2_ 2413354)
