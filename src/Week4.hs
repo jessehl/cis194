650afb2f98285dcecbcad2d7a0749ef56e3da49f
jessehl <j.loor@hotmail.com>
2024-12-29 10:22:37 +0100

    implement week 4 exercise 1

--- src/Week4.hs
+++ src/Week4.hs
@@ -1,6 +1,5 @@
 module Week4 where
 
-
 fun1 :: [Integer] -> Integer
 fun1 [] = 1
 fun1 (x:xs)
@@ -8,13 +7,15 @@ fun1 (x:xs)
   | otherwise = fun1 xs
 
 fun1_ :: [Integer] -> Integer
-fun1_ xs = product $ (\x -> x -2) <$> filter even xs
-
+fun1_ xs = product $ subtract 2 <$> filter even xs
 
 fun2 :: Integer -> Integer
 fun2 1 = 0
 fun2 n | even n = n + fun2 (n `div` 2)
        | otherwise = fun2 (3 * n + 1)
 
-fun2_ :: Integer -> Integer 
-fun2_ = fun2
+logic2 :: Integer  -> Integer 
+logic2 x = if even x then x `div` 2 else 3 * x + 1
+
+fun2_ :: Integer -> Integer
+fun2_ n = sum $ filter even $ takeWhile (>1) $ iterate logic2 n
