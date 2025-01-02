9c54f44abcafd49235cd6b5c535a1d0ba62ca364
jessehl <j.loor@hotmail.com>
2025-01-02 21:38:09 +0100

    exercise 3

--- app/Main.hs
+++ app/Main.hs
@@ -2,12 +2,15 @@ module Main where
 
 import Week2.Week2
 import Week2.Log (testParse)
-import Week3.Golf (histogram)
+import Week4(foldTree, maybeHead)
 import Data.Foldable (traverse_)
 
 main :: IO ()
 main = do
-   messages <- testParse parse 1000000 "resources/Week2/error.log"
+   messages <- testParse parse 1 "resources/Week2/error.log"
    traverse_ print (inorder (build messages))
-   putStr (histogram [1, 4,4,4,4, 5, 3])
+   let tree = foldTree "ABCDEFGHIJ"
+   print $ maybeHead tree 
+   print tree
+   
 
--- src/Week4.hs
+++ src/Week4.hs
@@ -14,8 +14,38 @@ fun2 1 = 0
 fun2 n | even n = n + fun2 (n `div` 2)
        | otherwise = fun2 (3 * n + 1)
 
-logic2 :: Integer  -> Integer 
+logic2 :: Integer  -> Integer
 logic2 x = if even x then x `div` 2 else 3 * x + 1
 
 fun2_ :: Integer -> Integer
 fun2_ n = sum $ filter even $ takeWhile (>1) $ iterate logic2 n
+
+data Tree a = Leaf
+  | Node Integer (Tree a) a (Tree a)
+  deriving (Show, Eq)
+
+
+-- Creates a balanced (but unsorted) Tree.
+foldTree :: [a] -> Tree a
+foldTree = foldr (\z a -> fst $ insert z a) Leaf
+
+-- Inserts an 'a' into the Tree, returning the new Tree, and the number of levels added by the insert.
+insert :: a -> Tree a -> (Tree a, Integer)
+insert a ta = case ta of
+  Leaf -> (Node 0 Leaf a Leaf, 1)
+  Node int left value right -> case (left, right) of
+    (Leaf, Leaf)                           -> (Node 1 newNode value right, 1)
+    (Leaf, Node{})                         -> (Node int newNode value right, 0)
+    (Node{}, Leaf)                         -> (Node int left value newNode, 0)
+    (Node l _ _ _, Node r _ _ _) |  l < r  -> (Node int newL value right, 0)
+    (Node l _ _ _, Node r _ _ _) |  l > r  -> (Node int left value newR, 0)
+    (Node {}, Node {})                     -> (Node (int + intL) newL value right, intL)
+    where
+      newNode     =  Node 0 Leaf a Leaf
+      (newL, intL) = insert a left
+      (newR, _)    = insert a right
+
+maybeHead :: Tree a -> Maybe (a, Integer)
+maybeHead t = case t of 
+   Leaf -> Nothing 
+   Node int _ a _ -> Just (a, int)
