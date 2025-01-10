eb6db478bd95d23166b0b51ce6d32d6ff4dfe25c
jessehl <j.loor@hotmail.com>
2025-01-10 10:20:12 +0100

    work on Expr type class + instance (hard-code the Integer for now)

--- src/Week5/Week5.hs
+++ src/Week5/Week5.hs
@@ -12,3 +12,22 @@ eval = \case
 
 evalStr :: String -> Maybe Integer
 evalStr str = eval <$> parseExp Lit Add Mul str
+
+-- Create a type class called Expr with three methods called lit, add,
+-- and mul which parallel the constructors of ExprT. Make an instance of
+-- Expr for the ExprT type, in such a way that
+-- mul (add (lit 2) (lit 3)) (lit 4) :: ExprT
+-- == Mul (Add (Lit 2) (Lit 3)) (Lit 4)
+
+class Expr a where
+    lit :: Integer -> a
+    add :: a -> a -> a
+    mul :: a -> a -> a
+
+
+instance Expr ExprT where
+    lit :: Integer -> ExprT
+    add = Add
+    mul = Mul
+    lit = Lit
+
