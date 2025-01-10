6c3d06169d6009a0923066dc1a437187f0d8388f
jessehl <j.loor@hotmail.com>
2025-01-10 08:03:19 +0100

    first exercise

--- src/Week5/Week5.hs
+++ src/Week5/Week5.hs
@@ -1,6 +1,10 @@
+{-# LANGUAGE LambdaCase #-}
 module Week5.Week5 where
 
 import Week5.ExprT
 
 eval :: ExprT -> Integer
-eval _ = 20
+eval = \case 
+    Lit nr -> nr
+    Mul a b -> eval a * eval b 
+    Add a b -> eval a + eval b 
