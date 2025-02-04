c008eef736bc2e73b669aa39d52cb63ec3b1b49b
jessehl <j.loor@hotmail.com>
2025-02-04 20:16:00 +0100

    start week 6

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,7 @@
 module Main where
 
-import Week5.Week5
+import Week6
 
 main :: IO ()
 main = do
-   print $ compileAndRun "(5 + (0 * (1 + 0) * 10 + -100 + -0 + 000 + 1 + -0 * 1)) * 10000"
+   print $ fib 6
