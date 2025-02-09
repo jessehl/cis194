48bc7605524b720610d361d34e52b31761f0e5b2
jessehl <j.loor@hotmail.com>
2025-02-09 13:27:50 +0100

    show Stream

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,8 @@
 module Main where
 
 import Week6
-import Data.Foldable (toList)
 
 main :: IO ()
 main = do
    let ones:: Stream Int = Cons 1 ones
-   print $ take 1000 $ toList ones
+   print ones
--- src/Week6.hs
+++ src/Week6.hs
@@ -1,4 +1,7 @@
 module Week6 where
+import Data.Foldable (Foldable(toList))
+import Data.List (intercalate)
+import GHC.Base (Opaque(O))
 
 fib :: Integer -> Integer
 
@@ -11,9 +14,12 @@ fibs1 = fmap fib [0..]
 
 fibs2 :: [Integer]
 fibs2 = 0 : go 0 1
-  where 
-    go n1 n2 = n3 : go n2 n3 
+  where
+    go n1 n2 = n3 : go n2 n3
        where n3 = n1 + n2
 
 
 data Stream a = Cons a (Stream a) deriving Foldable
+
+instance Show a => Show (Stream a) where
+    show stream =  "Stream(" ++ intercalate ", " (take 20 (fmap show (toList stream))) ++ ", ..., ∞)"
