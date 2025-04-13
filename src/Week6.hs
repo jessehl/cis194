4817b171f121481280da68e458ff1e7cf7691be2
jessehl <j.loor@hotmail.com>
2025-04-13 08:39:48 +0200

    start on Num instance of `Stream Integer`

--- src/Week6.hs
+++ src/Week6.hs
@@ -38,6 +38,19 @@ nats = streamFromSeed (+1) 1
 interleaveStreams :: Stream a -> Stream a -> Stream a
 interleaveStreams (Cons a1 remainder1) (Cons a2 remainder2) = Cons a1 (Cons a2 $ interleaveStreams remainder1 remainder2)
 
+zipStreams :: Stream a -> Stream b -> Stream (a, b)
+zipStreams streamA streamB = case (streamA, streamB) of
+  (Cons a ra, Cons b rb) -> Cons (a,b) (zipStreams ra rb)
 
-ruler :: Stream Integer 
+ruler :: Stream Integer
 ruler = Cons 0 (interleaveStreams (streamMap (+1) ruler) (streamRepeat 0))
+
+
+x :: Stream Integer
+x = 0 + (1 * x)
+
+instance Num (Stream Integer) where
+  fromInteger n = Cons n (streamRepeat 0)
+  negate = streamMap (*(-1))
+  (+) streamA streamB = streamMap (uncurry (+)) (zipStreams streamA streamB)
+
